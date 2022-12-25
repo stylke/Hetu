37b827a002787d982019ff5af7585a3edd7f2e77
tarccy <wnm010711@gmail.com>
2022-12-25 20:02:10 +0800

    modify loss module

--- python_refactor/hetu/nn/modules/loss.py
+++ python_refactor/hetu/nn/modules/loss.py
@@ -14,17 +14,14 @@ __all__ = [
 class _Loss(Module):
     reduction: str
 
-    def __init__(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+    def __init__(self, reduction: str = 'mean') -> None:
         super(_Loss, self).__init__()
-        if size_average is not None or reduce is not None:
-            self.reduction: str = ""
-        else:
-            self.reduction = reduction
+        self.reduction = reduction
 
 
 class _WeightedLoss(_Loss):
-    def __init__(self, weight: Optional[Tensor] = None, size_average=None, reduce=None, reduction: str = 'mean') -> None:
-        super(_WeightedLoss, self).__init__(size_average, reduce, reduction)
+    def __init__(self, weight: Optional[Tensor] = None, reduction: str = 'mean') -> None:
+        super(_WeightedLoss, self).__init__(reduction)
         self.register_buffer('weight', weight)
         self.weight: Optional[Tensor]
 
@@ -32,10 +29,8 @@ class _WeightedLoss(_Loss):
 class NLLLoss(_WeightedLoss):
     ignore_index: int
 
-    def __init__(self, weight: Optional[Tensor] = None, size_average=None, ignore_index: int = -100,
-                 reduce=None, reduction: str = 'mean') -> None:
-        super(NLLLoss, self).__init__(weight, size_average, reduce, reduction)
-        self.ignore_index = ignore_index
+    def __init__(self, weight: Optional[Tensor] = None, reduction: str = 'mean') -> None:
+        super(NLLLoss, self).__init__(weight, reduction)
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         return hetu.nll_loss(input, target)
@@ -43,10 +38,8 @@ class NLLLoss(_WeightedLoss):
 class BCELoss(_WeightedLoss):
     ignore_index: int
 
-    def __init__(self, weight: Optional[Tensor] = None, size_average=None, ignore_index: int = -100,
-                 reduce=None, reduction: str = 'mean') -> None:
-        super(BCELoss, self).__init__(weight, size_average, reduce, reduction)
-        self.ignore_index = ignore_index
+    def __init__(self, weight: Optional[Tensor] = None, reduction: str = 'mean') -> None:
+        super(BCELoss, self).__init__(weight, reduction)
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         return hetu.binary_cross_entroy(input, target)
@@ -54,10 +47,8 @@ class BCELoss(_WeightedLoss):
 class KLDivLoss(_WeightedLoss):
     ignore_index: int
 
-    def __init__(self, weight: Optional[Tensor] = None, size_average=None, ignore_index: int = -100,
-                 reduce=None, reduction: str = 'mean') -> None:
-        super(KLDivLoss, self).__init__(weight, size_average, reduce, reduction)
-        self.ignore_index = ignore_index
+    def __init__(self, weight: Optional[Tensor] = None, reduction: str = 'mean') -> None:
+        super(KLDivLoss, self).__init__(weight, reduction)
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         return hetu.kl_div(input, target)
@@ -65,10 +56,8 @@ class KLDivLoss(_WeightedLoss):
 class MSELoss(_WeightedLoss):
     ignore_index: int
 
-    def __init__(self, weight: Optional[Tensor] = None, size_average=None, ignore_index: int = -100,
-                 reduce=None, reduction: str = 'mean') -> None:
-        super(MSELoss, self).__init__(weight, size_average, reduce, reduction)
-        self.ignore_index = ignore_index
+    def __init__(self, weight: Optional[Tensor] = None, reduction: str = 'mean') -> None:
+        super(MSELoss, self).__init__(weight, reduction)
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         return hetu.mse_loss(input, target)
