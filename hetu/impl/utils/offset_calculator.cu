3b708ac76a8306e5f25b7db527562874b0d6cda6
Somoku <linsh0@protonmail.com>
2023-12-18 00:25:34 +0800

    refactor: add LFU cache for strided offset calculator caching to improve efficiency

--- hetu/impl/utils/offset_calculator.cu
+++ hetu/impl/utils/offset_calculator.cu
@@ -10,9 +10,11 @@ namespace {
 static std::once_flag offset_calculator_init_flag;
 static NDArray trivial_offset_calculator_arr;
 static OffsetCalculator* trivial_offset_calculator;
+static LFUCache lfu_cache;
 
 static void OffCalcInitOnce(const Stream& stream) {
   std::call_once(offset_calculator_init_flag, [](const Stream& stream) {
+    lfu_cache = LFUCache(HT_LFU_CAPACITY);
     CUDAStream cuda_stream(stream);
     trivial_offset_calculator_arr =
       NDArray::empty({static_cast<int64_t>(sizeof(OffsetCalculator))},
@@ -32,21 +34,30 @@ AllocOffsetCalculator(const NDArray& arr, const Stream& stream) {
   if (arr->is_contiguous()) {
     return {trivial_offset_calculator_arr, trivial_offset_calculator};
   }
-  auto device_id = arr->device().index();
-  CUDAStream cuda_stream(stream);
-  size_t ndim = arr->ndim();
-  size_t alloc_size = ndim * sizeof(int64_t);
-  auto shape_arr = 
-      hetu::cuda::to_int64_ndarray(arr->shape(), device_id);
-  auto stride_arr =
-      hetu::cuda::to_int64_ndarray(arr->stride(), device_id);
-  NDArray offset_calculator_arr = NDArray::empty({static_cast<int64_t>(sizeof(StridedOffsetCalculator))},
-                                                 Device(kCUDA, device_id), kByte, stream.stream_index());
-  auto offset_calculator = reinterpret_cast<StridedOffsetCalculator*>(offset_calculator_arr->raw_data_ptr());
-  hetu::cuda::CUDADeviceGuard guard(device_id);
-  strided_constructor<<<1, 1, 0, cuda_stream>>>(offset_calculator, ndim,
-                                                shape_arr->data_ptr<int64_t>(), stride_arr->data_ptr<int64_t>());
-  NDArray::MarkUsedBy({shape_arr, stride_arr}, stream);
+  NDArray offset_calculator_arr;
+  StridedOffsetCalculator* offset_calculator;
+  auto shape_arr_host = arr->shape();
+  auto stride_arr_host = arr->stride();
+  std::tie(offset_calculator_arr, offset_calculator) =
+      lfu_cache.get(shape_arr_host, stride_arr_host);
+  if (!offset_calculator) {
+    auto device_id = arr->device().index();
+    CUDAStream cuda_stream(stream);
+    size_t ndim = arr->ndim();
+    size_t alloc_size = ndim * sizeof(int64_t);
+    auto shape_arr = 
+        hetu::cuda::to_int64_ndarray(arr->shape(), device_id);
+    auto stride_arr =
+        hetu::cuda::to_int64_ndarray(arr->stride(), device_id);
+    offset_calculator_arr = NDArray::empty({static_cast<int64_t>(sizeof(StridedOffsetCalculator))},
+                                           Device(kCUDA, device_id), kByte, stream.stream_index());
+    offset_calculator = offset_calculator_arr->data_ptr<StridedOffsetCalculator>();
+    hetu::cuda::CUDADeviceGuard guard(device_id);
+    strided_constructor<<<1, 1, 0, cuda_stream>>>(offset_calculator, ndim,
+                                                  shape_arr->data_ptr<int64_t>(), stride_arr->data_ptr<int64_t>());
+    NDArray::MarkUsedBy({shape_arr, stride_arr}, stream);
+    lfu_cache.put(shape_arr_host, stride_arr_host, offset_calculator_arr, offset_calculator);
+  }
   return {offset_calculator_arr, offset_calculator};
 }
 
--- hetu/impl/utils/offset_calculator.cuh
+++ hetu/impl/utils/offset_calculator.cuh
@@ -2,9 +2,13 @@
 
 #include "hetu/core/ndarray.h"
 #include "hetu/core/memory_pool.h"
+#include "hetu/utils/robin_hood_hashing.h"
+#include "hetu/utils/unordered_dense.h"
 
 #include <tuple>
 #include <cassert>
+#include <unordered_map>
+#include <list>
 
 namespace hetu {
 namespace impl {
@@ -74,6 +78,133 @@ __global__ static void strided_constructor(StridedOffsetCalculator* dst, int dim
 
 std::tuple<NDArray, OffsetCalculator*>
 AllocOffsetCalculator(const NDArray& arr, const Stream& stream);
+
+// TODO: Find a suitable capacity for LFU cache
+constexpr size_t HT_LFU_CAPACITY = 32;
+
+struct CacheKey {
+  HTShape shape;
+  HTStride stride;
+
+  CacheKey() = default;
+  CacheKey(const HTShape& shape, const HTStride& stride) : shape(shape), stride(stride) {}
+
+  bool operator==(const CacheKey& other) const {
+    return shape == other.shape && stride == other.stride;
+  }
+};
+
+struct CacheKeyHash {
+  size_t operator()(const CacheKey& key) const {
+    size_t seed = 0;
+    auto hash_combine = [](size_t& seed, int64_t value) {
+      // Following boost::hash_combine
+      std::hash<int64_t> hasher;
+      seed ^= hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    };
+    for (auto i : key.shape) {
+      hash_combine(seed, i);
+    }
+    for (auto i : key.stride) {
+      hash_combine(seed, i);
+    }
+    return seed;
+  }
+};
+
+struct CacheValue {
+  NDArray offset_calculator_arr;
+  StridedOffsetCalculator* offset_calculator;
+
+  CacheValue() = default;
+
+  CacheValue(const NDArray& offset_calculator_arr, StridedOffsetCalculator* offset_calculator)
+    : offset_calculator_arr(offset_calculator_arr), offset_calculator(offset_calculator) {}
+};
+
+struct CacheNode {
+  CacheKey key;
+  CacheValue value;
+  size_t frequency;
+
+  CacheNode() = default;
+  CacheNode(const CacheKey& key, const CacheValue& value, size_t frequency)
+    : key(key), value(value), frequency(frequency) {}
+};
+
+class LFUCache {
+ public:
+  LFUCache() = default;
+  LFUCache(size_t capacity) : _capacity(capacity), _minFreq(0) {
+    _cache.reserve(capacity);
+    _freqList.reserve(capacity);
+  }
+
+  std::tuple<NDArray, StridedOffsetCalculator*> get(const HTShape& shape, const HTStride& stride) {
+    return get(CacheKey{shape, stride});
+  }
+
+  void put(const HTShape& shape, const HTStride& stride, const NDArray& offset_calculator_arr,
+           StridedOffsetCalculator* offset_calculator) {
+    put(CacheKey{shape, stride}, CacheValue{offset_calculator_arr, offset_calculator});
+  }
+
+ protected:
+  std::tuple<NDArray, StridedOffsetCalculator*> get(const CacheKey& key) {
+    if (_capacity == 0) return {NDArray(), nullptr};
+    auto it = _cache.find(key);
+    if (it == _cache.end()) return {NDArray(), nullptr};
+    auto node = it->second;
+    auto val = node->value;
+    auto freq = node->frequency;
+    // remove from `freq` list and push to `freq + 1` list
+    _freqList[freq].erase(node);
+    if (_freqList[freq].size() == 0) {
+      _freqList.erase(freq);
+      if (_minFreq == freq) _minFreq++;
+    }
+    _freqList[freq + 1].push_front(CacheNode(key, val, freq + 1));
+    _cache[key] = _freqList[freq + 1].begin();
+    return {val.offset_calculator_arr, val.offset_calculator};
+  }
+
+  void put(const CacheKey& key, const CacheValue& val) {
+    if (_capacity == 0) return;
+    auto it = _cache.find(key);
+    if (it == _cache.end()) {
+      // evict when cache is full
+      if (_cache.size() == _capacity) {
+        auto evict_it = _freqList[_minFreq].back();
+        _cache.erase(evict_it.key);
+        _freqList[_minFreq].pop_back();
+        if (_freqList[_minFreq].size() == 0) {
+          _freqList.erase(_minFreq);
+        }
+      }
+      _freqList[1].push_front(CacheNode(key, val, 1));
+      _cache[key] = _freqList[1].begin();
+      _minFreq = 1;
+    } else if (!val.offset_calculator_arr.is_defined()){
+      // update value
+      auto node = it->second;
+      auto freq = node->frequency;
+      _freqList[freq].erase(node);
+      if (_freqList[freq].size() == 0) {
+        _freqList.erase(freq);
+        if (_minFreq == freq) _minFreq++;
+      }
+      _freqList[freq + 1].push_front(CacheNode(key, val, freq + 1));
+      _cache[key] = _freqList[freq + 1].begin();
+    } else {
+      HT_RUNTIME_ERROR << "OffsetCalculator already exists in cache.";
+    }
+  }
+
+  size_t _capacity;
+  size_t _minFreq;
+  ankerl::unordered_dense::map<CacheKey, std::list<CacheNode>::iterator, CacheKeyHash> _cache;
+  ankerl::unordered_dense::map<size_t, std::list<CacheNode>, ankerl::unordered_dense::hash<size_t>> _freqList;
+};
 
 } // namespace impl
 } // namespace hetu
