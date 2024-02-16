537e0c02af8756a5007a6322bfdcf27dc142451e
gehao <sakura.gehao@gmail.com>
2024-02-16 22:31:11 +0800

    add: hetu pack or pad script

---
+++ examples/nlp/gpt/scripts/hetu_pack_or_pad.sh
@@ -0,0 +1,43 @@
+# NCCL_DEBUG=info
+NUM_LAYERS=${1:-32}
+HIDDEN_SIZE=${2:-1280}
+NUM_HEADS=${3:-32}
+SEQ_LEN=${4:-128}
+GLOBAL_BATCH_SIZE=${5:-16}
+MICRO_BATCH_SIZE=${6:-2}
+
+ROOT_FOLDER=/home/gehao/megatron/Megatron-LM/data
+JSON_FILE=${ROOT_FOLDER}/web/refinedweb0.json
+JSON_KEY=content
+VOCAB_FILE=${ROOT_FOLDER}/vocab.json
+MERGE_FILE=${ROOT_FOLDER}/merges.txt
+
+export NCCL_DEBUG=VERSION
+export HETU_SWITCH_ALGORITHM=ROUND_ROBIN
+export HETU_SWITCH_PROFILE=INFO
+export HETU_INTERNAL_LOG_LEVEL=WARN
+mpirun --allow-run-as-root -np 8 \
+--output-filename logs/ds_parallel --merge-stderr-to-stdout \
+python3 hetu_pack_or_pad.py \
+--num_strategy=1 \
+--ds_parallel_config ds_parallel_config/dp2_tp2_pp2.json \
+--global_batch_size $GLOBAL_BATCH_SIZE \
+--micro_batch_size $MICRO_BATCH_SIZE \
+--json_file $JSON_FILE \
+--json_key $JSON_KEY \
+--vocab_file $VOCAB_FILE \
+--merge_file $MERGE_FILE \
+--vocab_size 30592 \
+--hidden_size $HIDDEN_SIZE \
+--num_hidden_layers $NUM_LAYERS \
+--num_attention_heads $NUM_HEADS \
+--seq_length $SEQ_LEN \
+--epochs 4 \
+--steps 40 \
+--lr 1e-4 \
+--adam_weight_decay 0.01 \
+--hidden_act relu \
+--dropout_prob 0.1 \
+--bf16 \
+--use_flash_attn \
+
