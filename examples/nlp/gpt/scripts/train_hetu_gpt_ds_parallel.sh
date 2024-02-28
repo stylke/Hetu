efeeb63baf0c6b57a2fa8d16c862fa5c79841a49
gehao <sakura.gehao@gmail.com>
2024-02-28 21:41:49 +0800

    add parallel config auto generate script

--- examples/nlp/gpt/scripts/train_hetu_gpt_ds_parallel.sh
+++ examples/nlp/gpt/scripts/train_hetu_gpt_ds_parallel.sh
@@ -4,6 +4,10 @@ NUM_HEADS=${3:-32}
 SEQ_LEN=${4:-128}
 GLOBAL_BATCH_SIZE=${5:-8}
 MICRO_BATCH_SIZE=${6:-2}
+DP=${7:-2}
+TP=${8:-2}
+PP=${9:-2}
+NUM_GPUS=$(( $DP * $TP *$PP ))
 
 ROOT_FOLDER=/opt/tiger/test/Hetu-dev/examples/nlp/gpt/data
 JSON_FILE=${ROOT_FOLDER}/web/refinedweb0.json
@@ -11,12 +15,13 @@ JSON_KEY=content
 VOCAB_FILE=${ROOT_FOLDER}/vocab.json
 MERGE_FILE=${ROOT_FOLDER}/merges.txt
 
+# ds_parallel_config can use ds_parallel_config/generate_gpt_3d_config.py for auto-generate
 export NCCL_DEBUG=VERSION
 export HETU_INTERNAL_LOG_LEVEL=WARN
 mpirun --allow-run-as-root -np 8 \
 --output-filename logs/ds_parallel --merge-stderr-to-stdout \
 python3 train_hetu_gpt_ds_parallel.py \
---ds_parallel_config ds_parallel_config/dp2_tp2_pp2.json \
+--ds_parallel_config ds_parallel_config/gpus${NUM_GPUS}/dp${DP}_tp${TP}_pp${PP}.json \
 --global_batch_size $GLOBAL_BATCH_SIZE \
 --micro_batch_size $MICRO_BATCH_SIZE \
 --json_file $JSON_FILE \
