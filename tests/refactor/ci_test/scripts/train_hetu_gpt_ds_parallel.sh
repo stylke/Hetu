6a8b183780a46886c0d206e09c53d9b76bf0c9f9
Somoku <linsh0@protonmail.com>
2024-04-01 16:28:52 +0800

    merge from 'ci_test' branch

---
+++ tests/refactor/ci_test/scripts/train_hetu_gpt_ds_parallel.sh
@@ -0,0 +1,44 @@
+NUM_LAYERS=${1:-32}
+HIDDEN_SIZE=${2:-768}
+NUM_HEADS=${3:-32}
+SEQ_LEN=${4:-128}
+GLOBAL_BATCH_SIZE=${5:-8}
+MICRO_BATCH_SIZE=${6:-2}
+DP=${7:-2}
+TP=${8:-1}
+PP=${9:-2}
+NUM_GPUS=$(( $DP * $TP *$PP ))
+
+ROOT_FOLDER=/home/gehao/njw1123/Hetu-dev_test/tests/refactor/ci_test/data_utils/data
+JSON_FILE=${ROOT_FOLDER}/web/refinedweb0.json
+JSON_KEY=content
+VOCAB_FILE=${ROOT_FOLDER}/vocab.json
+MERGE_FILE=${ROOT_FOLDER}/merges.txt
+
+# ds_parallel_config can use ds_parallel_config/generate_gpt_3d_config.py for auto-generate
+export NCCL_DEBUG=VERSION
+export HETU_INTERNAL_LOG_LEVEL=WARN
+CUDA_VISIBLE_DEVICES=2,3,4,5, mpirun --allow-run-as-root -np 4 \
+--output-filename logs/ds_parallel --merge-stderr-to-stdout \
+python3 train_hetu_gpt_ds_parallel.py \
+--ds_parallel_config ds_parallel_config/gpus${NUM_GPUS}/dp${DP}_tp${TP}_pp${PP}.json \
+--global_batch_size $GLOBAL_BATCH_SIZE \
+--micro_batch_size $MICRO_BATCH_SIZE \
+--json_file $JSON_FILE \
+--json_key $JSON_KEY \
+--vocab_file $VOCAB_FILE \
+--merge_file $MERGE_FILE \
+--vocab_size 30592 \
+--hidden_size $HIDDEN_SIZE \
+--num_hidden_layers $NUM_LAYERS \
+--num_attention_heads $NUM_HEADS \
+--seq_length $SEQ_LEN \
+--epochs 10 \
+--steps 50 \
+--lr 1e-6 \
+--adam_weight_decay 0.01 \
+--hidden_act relu \
+--dropout_prob 0.1 \
+--bf16 \
+--use_flash_attn \
+# --profiler \
